fd6288272e7567bd677718205d7597fa124edcd3
Lukáš Dřevo <lukas.drevo01@gmail.com>
2025-05-25 18:00:33 +0200

    kll

--- ProjektWPF/ProjektWPF/MainWindow.xaml.cs
+++ ProjektWPF/ProjektWPF/MainWindow.xaml.cs
@@ -98,7 +98,52 @@ private void Button_Click(object sender, RoutedEventArgs e)
             
         }
     }
+    public class LangToCountryConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                try
+                {
+                    RegionInfo r = new RegionInfo((int)value);
+                    return r.TwoLetterISORegionName;
+                }
+                catch
+                {
+                    return ""; // Invariant, etc.
+                }
+            }
+            else return ""; // Default
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    // Converters pipelining
+    public class CombiningConverter : IValueConverter
+    {
+        public IValueConverter Converter1 { get; set; }
+        public IValueConverter Converter2 { get; set; }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object convertedValue = Converter1.Convert(value, targetType, parameter, culture);
+            return Converter2.Convert(convertedValue, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
 
 
-    
 }
